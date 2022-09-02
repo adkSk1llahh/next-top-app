bee97dbc4e8523c937526d6059aad8b5455587fa
Adilkhan <sk1llahh2@gmail.com>
2022-09-02 12:03:13 +0600

    08/07

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import {RatingProps} from "./Rating.props";
+import cn from 'classnames'
+import styles from './Rating.module.css'
+import StarIcon from './star.svg'
+
+export const Rating = ({isEditable = false, rating, setRating, className, ...props}: RatingProps): JSX.Element => {
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>))
+
+  useEffect(() => {
+    constructRating(rating)
+  }, [rating])
+
+  const constructRating = (currentRating: number) => {
+    const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
+      return <StarIcon className={cn(styles.star, {
+        [styles.filled]: i < currentRating
+      })}
+
+      />
+    })
+
+    setRatingArray(updatedArray)
+  }
+
+  return (
+    <div {...props}>
+      {ratingArray.map((r, i) => (
+        <span key={i}>
+          {r}
+        </span>
+      ))}
+    </div>
+  );
+};
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Htag} from "../components";
+import {Button, Htag, P, Rating, Tag} from "../components";
 
 
 export default function Home() {
@@ -7,7 +7,11 @@ export default function Home() {
     <div>
     <Htag tag='h1'>qwe</Htag>
     <Button appearance='primary' className='qweqwe'>qwe</Button>
-    <Button appearance='ghost'>qwe</Button>
+    <Button appearance='ghost' arrow='down'>qwe</Button>
+    <Tag size='s' color='red'>qwe</Tag>
+    <Tag size='s' color='green'>qwe</Tag>
+    <Tag size='s' color='primary'>qwe</Tag>
+      <Rating rating={3}/>
     </div>
   );
 }
