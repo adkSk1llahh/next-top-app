24858e399004046acbee46e69eca742e804d7c24
Adilkhan <sk1llahh2@gmail.com>
2022-09-02 16:44:38 +0600

    10/08

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,24 @@
 import {FooterProps} from "./Footer.props";
 import React from "react";
+import cn from 'classnames';
+import styles from './Footer.module.css'
+import {format} from 'date-fns'
 
-export const Footer = ({...props}: FooterProps):JSX.Element => {
+
+export const Footer = ({className,...props}: FooterProps):JSX.Element => {
   return(
-    <div {...props}>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+      </div>
 
-    </div>
+      <div>
+        Пользовательское соглашение
+      </div>
+
+      <div>
+        Политика конфиденциальности
+      </div>
+    </footer>
   )
 }
