4d4bead7da5c6cacfb207e0805faf3b7557059ce
Adilkhan <sk1llahh2@gmail.com>
2022-10-04 16:40:20 +0600

    15/03

--- page-components/TopPageComponent/TopPageComponent.props.ts
+++ page-components/TopPageComponent/TopPageComponent.props.ts
@@ -4,5 +4,5 @@ import {ProductModel} from "../../interfaces/product.interface";
 export interface TopPageComponentProps{
   firstCategory: TopLevelCategory,
   page: TopPageModel,
-  products: ProductModel
+  products: ProductModel[]
 }
