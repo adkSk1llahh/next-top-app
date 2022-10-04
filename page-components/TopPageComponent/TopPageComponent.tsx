328d3d66ce3fcaabdb754d629bd7a93958d73863
Adilkhan <sk1llahh2@gmail.com>
2022-10-04 12:54:10 +0600

    14/05

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,11 +1,69 @@
 import {TopPageComponentProps} from "./TopPageComponent.props";
+import React from "react";
+import {Htag, P, Tag} from "../../components";
+import styles from './TopPageComponent.module.css'
+import {TopLevelCategory} from "../../interfaces/page.interface";
+import {HhData} from "../../components/HhData/HhData";
+import {Advantages} from "../../components/Advantages/Advantages";
 
 export const TopPageComponent = ({page, products, firstCategory}:TopPageComponentProps):JSX.Element => {
   return(
-    <>
-      {
-        products && products.length
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag='h1'>
+          {page.title}
+        </Htag>
+        {
+          products &&
+          <Tag color='grey' size='m'>
+            {products.length}
+          </Tag>
+        }
+
+        <span>Сортировка</span>
+
+      </div>
+
+      <div>
+        {products && products.map(p => (
+          <div key={p._id}>
+            {p.title}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.hhTitle}>
+        <Htag tag='h2'>
+          Вакансии -{page.category}
+        </Htag>
+        <Tag color='red' size='m'>
+          hh.ru
+        </Tag>
+      </div>
+
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+      {console.log(page)}
+      {page.advantages && page.advantages.length > 0 &&
+      <>
+        <Htag tag='h2'>
+          Приемущества
+        </Htag>
+
+        <Advantages advantages={page.advantages}/>
+      </>
       }
-    </>
+
+      {
+        page.seoText && <P>{page.seoText}</P>
+      }
+
+      <Htag tag='h2'>
+        Получаемые навыки
+      </Htag>
+      {
+        page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)
+      }
+
+    </div>
   )
 }
