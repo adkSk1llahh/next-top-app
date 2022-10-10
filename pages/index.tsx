0d574b5e8c3e96a294efe8004dba3810d349abec
Adilkhan <sk1llahh2@gmail.com>
2022-10-10 12:53:18 +0600

    19/03

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,49 @@
-import React, {useState} from "react";
-import {Button, Htag, P, Rating, Tag} from "../components";
-import {withLayout} from "../layout/Layout";
-import {GetStaticProps} from "next";
+import { GetStaticProps } from 'next';
+import React, { useState } from 'react';
+import { Button, Htag, Input, P, Rating, Tag, Textarea } from '../components';
+import { withLayout } from '../layout/Layout';
 import axios from 'axios';
-import {MenuItem} from "../interfaces/menu.interface";
+import { MenuItem } from '../interfaces/menu.interface';
+import { API } from '../helpers/api';
 
+function Home({ menu }: HomeProps): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 
-const Home = ({menu, firstCategory}:HomeProps) => {
-  console.log(menu)
-
-  const [rating, setRating] = useState<number>(4)
-  return (
-    <>
-      <Htag tag='h1'>qwe</Htag>
-      <Button appearance='primary' className='qweqwe'>qwe</Button>
-      <Button appearance='ghost' arrow='down'>qwe</Button>
-      <Tag size='s' color='red'>qwe</Tag>
-      <Tag size='s' color='green'>qwe</Tag>
-      <Tag size='s' color='primary'>qwe</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+			<Input placeholder='тест' />
+			<Textarea placeholder='тест area' />
+		</>
+	);
 }
 
-export default withLayout(Home)
+export default withLayout(Home);
 
-export const getStaticProps: GetStaticProps = async () => {
-  const firstCategory = 0
-  const {data:menu} = await axios.post<MenuItem>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-    firstCategory
-  })
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
 
-  return{
-    props:{
-      menu,
-      firstCategory
-    }
-  }
-}
-
-
-interface HomeProps extends Record<string, unknown>{
-  menu: MenuItem[]
-  firstCategory: number
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
 }
